2adb16f6da22dcce63ab841db945b304ddb61778
Tim Patten <timpatten.syd@gmail.com>
2020-03-13 21:32:05 +0100

    Trying to get the hand mask

--- vis_HO3D_test.py
+++ vis_HO3D_test.py
@@ -145,36 +145,79 @@ if __name__ == '__main__':
             # apply current pose to the object model
             objMesh.v = np.matmul(objMesh.v, cv2.Rodrigues(anno['objRot'])[0].T) + anno['objTrans']
 
-            '''
+            # # ----- OBJECT
+
             obj_uv = projectPoints(objMesh.v, anno['camMat'])
 
             #obj_uv2 = project_3D_points(anno['camMat'], objMesh.v, is_OpenGL_coords=True)
 
-            for uv in obj_uv:
-                img[int(uv[1]), 640 - int(uv[0])] = [100, 100, 100]
+            #for uv in obj_uv:
+            #    img[int(uv[1]), 640 - int(uv[0])] = [100, 100, 100]
 
             import open3d
+            import copy
+
+            # mask = copy.deepcopy(img)
+            obj_mask = np.zeros((img.shape[0], img.shape[1]))
+            for uv in obj_uv:
+                obj_mask[int(uv[1]), 640 - int(uv[0])] = 255
+
+            cv2.imshow("Object Mask", obj_mask)
+
+            kernel = np.ones((5, 5), np.uint8)
+            obj_closing = cv2.morphologyEx(obj_mask, cv2.MORPH_CLOSE, kernel)
+            #cv2.imshow("Closing", closing)
+
+            obj_contour, _ = cv2.findContours(np.uint8(obj_closing), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+            obj_contour_image = copy.deepcopy(img)
+            obj_contour_image = cv2.drawContours(obj_contour_image, obj_contour, -1, (255, 0, 0), 2, lineType=cv2.LINE_AA)
+            cv2.imshow("Object Contour", obj_contour_image)
+
+            # # ----- HAND
             if hasattr(handMesh, 'r'):
                 hand_vertices = open3d.utility.Vector3dVector(np.copy(handMesh.r))
             elif hasattr(handMesh, 'v'):
                 hand_vertices = open3d.utility.Vector3dVector(np.copy(handMesh.v))
+
             hand_uv = projectPoints(hand_vertices, anno['camMat'])
-            for uv in np.asarray(hand_uv):
-                img[int(uv[1]), 640 - int(uv[0])] = [255, 0, 0]
+            hand_mask = np.zeros((img.shape[0], img.shape[1]))
+            for uv in hand_uv:
+                hand_mask[int(uv[1]), 640 - int(uv[0])] = 255
 
-            # create matplotlib window
-            fig = plt.figure(figsize=(1, 1))
-            figManager = plt.get_current_fig_manager()
-            figManager.resize(*figManager.window.maxsize())
+            cv2.imshow("Hand Mask", hand_mask)
 
-            # show RGB image
-            ax0 = fig.add_subplot(1, 1, 1)
-            ax0.imshow(img[:, :, [2, 1, 0]])
-            ax0.title.set_text('RGB Image')
+            # for each point, find its nearest and create a new point that is their mean
+            hand_vertices = np.asarray(hand_vertices)
+            hand_pcd = open3d.geometry.PointCloud()
+            hand_pcd.points = open3d.utility.Vector3dVector(hand_vertices)
+            kd_tree = open3d.geometry.KDTreeFlann(hand_pcd)
+            for i in range(3):
+                new_vertices = copy.deepcopy(hand_vertices)
+                for j in range(hand_vertices.shape[0]):
+                    _, idx, _ = kd_tree.search_knn_vector_3d(hand_vertices[j], 2)
+                    mid_point = 0.5 * (hand_vertices[j] + hand_vertices[idx[1]])
+                    new_vertices[j] = mid_point
+                hand_vertices = np.vstack((hand_vertices, new_vertices))
 
-            plt.show()
+            hand_uv = projectPoints(hand_vertices, anno['camMat'])
+            hand_mask = np.zeros((img.shape[0], img.shape[1]))
+            for uv in hand_uv:
+                hand_mask[int(uv[1]), 640 - int(uv[0])] = 255
+
+            cv2.imshow("Hand Mask 2", hand_mask)
+
+            kernel = np.ones((10, 10), np.uint8)
+            hand_closing = cv2.morphologyEx(hand_mask, cv2.MORPH_CLOSE, kernel)
+            cv2.imshow("Hand Closing", hand_closing)
+
+            hand_contour, _ = cv2.findContours(np.uint8(hand_closing), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+            hand_contour_image = copy.deepcopy(img)
+            hand_contour_image = cv2.drawContours(hand_contour_image, hand_contour, -1, (255, 0, 0), 2, lineType=cv2.LINE_AA)
+            cv2.imshow("Hand Contour", hand_contour_image)
+
+            cv2.waitKey(0)
+
             '''
-
             import open3d as o3d
             if hasattr(handMesh, 'r'):
                 hand_vertices = o3d.utility.Vector3dVector(np.copy(handMesh.r))
@@ -199,18 +242,11 @@ if __name__ == '__main__':
                 elif hasattr(m, 'v'):
                     mesh.vertices = o3d.utility.Vector3dVector(np.copy(m.v))
                     numVert = m.v.shape[0]
-                else:
-                    raise Exception('Unknown Mesh format')
                 mesh.triangles = o3d.utility.Vector3iVector(np.copy(m.f))
                 if colorList[i] == 'r':
-                    mesh.vertex_colors = o3d.utility.Vector3dVector(
-                        np.tile(np.array([[0.6, 0.2, 0.2]]), [numVert, 1]))
+                    mesh.vertex_colors = o3d.utility.Vector3dVector(np.tile(np.array([[0.6, 0.2, 0.2]]), [numVert, 1]))
                 elif colorList[i] == 'g':
-                    mesh.vertex_colors = o3d.utility.Vector3dVector(
-                        np.tile(np.array([[0.5, 0.5, 0.5]]), [numVert, 1]))
-                else:
-                    raise Exception('Unknown mesh color')
-
+                    mesh.vertex_colors = o3d.utility.Vector3dVector(np.tile(np.array([[0.5, 0.5, 0.5]]), [numVert, 1]))
                 vis.add_geometry(mesh)
 
             depth_vis = vis.capture_depth_float_buffer(do_render=True)
@@ -223,13 +259,22 @@ if __name__ == '__main__':
             #vis.destroy_window()
 
             import copy
-            mask = copy.deepcopy(img)
+            # mask = copy.deepcopy(img)
+            mask = np.zeros((img.shape[0], img.shape[1]))
             for u in range(depth_np.shape[0]):
                 for v in range(depth_np.shape[1]):
                     if depth_np[u, v] > 0:
-                        mask[u, v] = [100, 100, 100]
-                        mask[u + 35, v + 75] = [0, 0, 255]
+                        # mask[u, v] = 255
+                        mask[u + 35, v + 75] = 255
+                        # mask[u + 35, v + 75] = [0, 0, 255]
+            contour, _ = cv2.findContours(np.uint8(mask), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+            img_cont = cv2.drawContours(img, contour, -1, (255, 0, 0), 2, lineType=cv2.LINE_AA)
 
+            cv2.imshow("Image", img_cont)
+            cv2.waitKey(0)
+            '''
+
+            '''
             # create matplotlib window
             fig = plt.figure(figsize=(1, 2))
             figManager = plt.get_current_fig_manager()
@@ -243,6 +288,7 @@ if __name__ == '__main__':
             ax0.imshow(mask[:, :, [2, 1, 0]])
 
             plt.show()
+            '''
 
         elif args['visType'] == 'matplotlib':
 
