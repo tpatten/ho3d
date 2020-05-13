deba9f82f100cffbaee03bc66474e8d6cd55f387
Tim Patten <timpatten.syd@gmail.com>
2020-05-13 19:19:35 +0200

    Trying to align observations with ICP

--- visualize_camera_trajectory.py
+++ visualize_camera_trajectory.py
@@ -3,11 +3,11 @@
 
 import argparse
 from utils.grasp_utils import *
-from mano.webuser.smpl_handpca_wrapper_HAND_only import load_model
 import cv2
 import open3d as o3d
-import matplotlib.pyplot as plt
 import copy
+import transforms3d as tf3d
+
 
 HAND_MASK_DIR = 'hand'
 OBJECT_MASK_DIR = 'object'
@@ -25,6 +25,8 @@ class TrajectoryVisualizer:
         self.depth = None
         self.anno = None
         self.scene_pcd = None
+        self.prev_cloud = None
+        self.prev_pose = np.eye(4)
         if args.mask_erosion_kernel > 0:
             self.erosion_kernel = np.ones((args.mask_erosion_kernel, args.mask_erosion_kernel), np.uint8)
         else:
@@ -35,10 +37,11 @@ class TrajectoryVisualizer:
 
     def visualize_trajectory(self):
         # Get the scene ids
-        frame_ids = os.listdir(os.path.join(self.base_dir, self.data_split, self.args.scene, 'rgb'))
+        frame_ids = sorted(os.listdir(os.path.join(self.base_dir, self.data_split, self.args.scene, 'rgb')))
 
         # For each frame in the directory
         cam_poses = []
+        est_cam_poses = []
         mask_pcds = []
         scene_pcds = []
         processed_frames = []
@@ -65,11 +68,15 @@ class TrajectoryVisualizer:
             cloud, colors = self.image_to_world(mask, cut_z=np.linalg.norm(self.anno['objTrans'])*1.1)
 
             # Transform the cloud and get the camera position
-            cloud, cam_pose = self.transform_to_object_frame(cloud)
+            # cloud, cam_pose = self.transform_to_object_frame(cloud)
+            cloud, cam_pose, est_cloud, est_cam_pose = self.transform_to_object_frame(cloud)
 
             # Create the point cloud for visualization
             mask_pcd = o3d.geometry.PointCloud()
-            mask_pcd.points = o3d.utility.Vector3dVector(cloud)
+            if self.args.use_gt:
+                mask_pcd.points = o3d.utility.Vector3dVector(cloud)
+            else:
+                mask_pcd.points = o3d.utility.Vector3dVector(est_cloud)
             mask_pcd.colors = o3d.utility.Vector3dVector(colors)
             # mask_pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
 
@@ -78,6 +85,7 @@ class TrajectoryVisualizer:
 
             # Add to lists
             cam_poses.append(cam_pose)
+            est_cam_poses.append(est_cam_pose)
             mask_pcds.append(mask_pcd)
             scene_pcds.append(scene_pcd)
             processed_frames.append(frame_id)
@@ -86,6 +94,10 @@ class TrajectoryVisualizer:
             counter += self.args.skip
             num_processed += 1
 
+            # Update the previous cloud and pose
+            self.prev_cloud = cloud
+            self.prev_pose = cam_pose
+
             # Exit if reached the limit
             if self.args.max_num > 0 and num_processed == self.args.max_num:
                 break
@@ -93,12 +105,15 @@ class TrajectoryVisualizer:
         # Save
         if self.args.save:
             base_dir = os.path.join(self.base_dir, self.data_split, self.args.scene)
-            self.save_clouds_and_camera_poses(base_dir, processed_frames, cam_poses, mask_pcds)
+            self.save_clouds_and_camera_poses(base_dir, processed_frames, cam_poses, est_cam_poses, mask_pcds)
 
         # Visualize
         if self.args.visualize:
             scene_pcds = None
-            self.visualize(cam_poses, mask_pcds, scene_pcds)
+            if self.args.use_gt:
+                self.visualize(cam_poses, mask_pcds, scene_pcds=scene_pcds)
+            else:
+                self.visualize(est_cam_poses, mask_pcds, scene_pcds=scene_pcds, gt_poses=cam_poses)
 
     def load_data(self, seq_name, frame_id):
         rgb = read_RGB_img(self.base_dir, seq_name, frame_id, self.data_split)
@@ -143,6 +158,7 @@ class TrajectoryVisualizer:
         return pts, colors
 
     def transform_to_object_frame(self, cloud):
+        # Get the ground truth transformation
         obj_trans = np.copy(self.anno['objTrans'])
         obj_trans = obj_trans.dot(COORD_CHANGE_MAT.T)
         obj_rot = np.copy(self.anno['objRot'])
@@ -157,7 +173,46 @@ class TrajectoryVisualizer:
         cam_pose[:3, :3] = rot_max
         cam_pose[:3, 3] = np.matmul(-obj_trans, np.linalg.inv(rot_max))
 
-        return cloud_tfd, cam_pose
+        if self.args.use_gt or self.prev_cloud is None:
+            return cloud_tfd, cam_pose, cloud_tfd, cam_pose
+        else:
+            est_cam_pose = np.copy(self.align_icp(cloud))
+            est_cloud_tfd = np.copy(cloud)
+            est_cloud_tfd -= est_cam_pose[0:3, 3]
+            est_cloud_tfd = np.matmul(est_cloud_tfd, np.linalg.inv(est_cam_pose[0:3, 0:3]))
+            euler_gt = tf3d.euler.mat2euler(cam_pose[:3, :3])
+            trans_gt = cam_pose[:3, 3]
+            euler_est = tf3d.euler.mat2euler(est_cam_pose[:3, :3])
+            trans_est = est_cam_pose[:3, 3]
+            print('X {:.4f}\tY {:.4f}\tZ {:.4f}\tRx {:.4f}\tRy {:.4f}\tRz {:.4f}'.format(
+                abs(trans_gt[0] - trans_est[0]) * 100, abs(trans_gt[1] - trans_est[1]) * 100,
+                abs(trans_gt[2] - trans_est[2]) * 100,
+                np.degrees(abs(euler_gt[0] - euler_est[0])), np.degrees(abs(euler_gt[1] - euler_est[1])),
+                np.degrees(abs(euler_gt[2] - euler_est[2]))))
+            return cloud_tfd, cam_pose, est_cloud_tfd, est_cam_pose
+
+    def align_icp(self, cloud):
+        threshold = 0.02
+        trans_init = self.prev_pose
+
+        source_pcd = o3d.geometry.PointCloud()
+        source_pcd.points = o3d.utility.Vector3dVector(copy.deepcopy(cloud))
+        target_pcd = o3d.geometry.PointCloud()
+        target_pcd.points = o3d.utility.Vector3dVector(copy.deepcopy(self.prev_cloud))
+
+        #print("Initial alignment")
+        evaluation = o3d.registration.evaluate_registration(source_pcd, target_pcd, threshold, trans_init)
+        #print(evaluation)
+
+        #print("Apply point-to-point ICP")
+        reg_p2p = o3d.registration.registration_icp(source_pcd, target_pcd, threshold, trans_init,
+                                                    o3d.registration.TransformationEstimationPointToPoint())
+        #print(reg_p2p)
+        #print("Transformation is:")
+        #print(reg_p2p.transformation)
+        #print("")
+
+        return reg_p2p.transformation
 
     def remove_outliers(self, cloud):
         if self.args.outlier_rm_nb_neighbors > 0 and self.args.outlier_rm_std_ratio > 0:
@@ -170,7 +225,7 @@ class TrajectoryVisualizer:
             return cloud
 
     @staticmethod
-    def visualize(cam_poses, mask_pcds, scene_pcds=None):
+    def visualize(cam_poses, mask_pcds, scene_pcds=None, gt_poses=None):
         # Create visualizer
         vis = o3d.visualization.Visualizer()
         vis.create_window()
@@ -190,7 +245,7 @@ class TrajectoryVisualizer:
         # Plot camera pose
         for m in cam_poses:
             points = m[:3, :].T
-            points[0:3, :] *= 0.25
+            points[0:3, :] *= 0.175
             points[0:3, :] += np.tile(points[3, :], (3, 1))
             lines = [[3, 0], [3, 1], [3, 2]]
             line_colors = [[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]]
@@ -200,11 +255,26 @@ class TrajectoryVisualizer:
             line_set.colors = o3d.utility.Vector3dVector(line_colors)
             vis.add_geometry(line_set)
 
+        # Plot the ground truth camera poses
+        if gt_poses is not None:
+            for m in gt_poses:
+                points = m[:3, :].T
+                points[0:3, :] *= 0.1
+                points[0:3, :] += np.tile(points[3, :], (3, 1))
+                lines = [[3, 0], [3, 1], [3, 2]]
+                line_colors = [[.6, .4, .4], [.4, .6, .4], [.4, .4, .6]]
+                line_set = o3d.geometry.LineSet()
+                line_set.points = o3d.utility.Vector3dVector(points)
+                line_set.lines = o3d.utility.Vector2iVector(lines)
+                line_set.colors = o3d.utility.Vector3dVector(line_colors)
+                vis.add_geometry(line_set)
+
+        # End visualizer
         vis.run()
         vis.destroy_window()
 
     @staticmethod
-    def save_clouds_and_camera_poses(base_dir, frame_ids, cam_poses, mask_pcds):
+    def save_clouds_and_camera_poses(base_dir, frame_ids, cam_poses, est_cam_poses, mask_pcds):
         all_points = np.asarray(mask_pcds[0].points)
         all_colors = np.asarray(mask_pcds[0].colors)
         for i in range(1, len(mask_pcds)):
@@ -245,12 +315,14 @@ if __name__ == '__main__':
     # Parse the arguments
     parser = argparse.ArgumentParser(description='HANDS19 - Task#3 HO-3D Camera trajectory visualization')
     args = parser.parse_args()
-    args.ho3d_path = '/home/tpatten/v4rtemp/datasets/HandTracking/HO3D_v2/'
+    # args.ho3d_path = '/home/tpatten/v4rtemp/datasets/HandTracking/HO3D_v2/'
+    args.ho3d_path = '/home/tpatten/Data/Hands/HO3D/'
     args.scene = 'ABF10'
     args.visualize = True
-    args.save = True
-    args.max_num = -1
-    args.skip = 100
+    args.save = False
+    args.use_gt = False
+    args.max_num = 50
+    args.skip = 1  # 100
     args.mask_erosion_kernel = 5
     args.outlier_rm_nb_neighbors = 500
     args.outlier_rm_std_ratio = 0.001
